0a67ae50c1c16dcbcb72569cc33a2348b55eed19
Anvay Mandar Joshi <anvayj@cybage.com>
2020-10-20 14:07:59 +0530

    Testing CI/CD

--- src/components/Post.js
+++ src/components/Post.js
@@ -7,7 +7,6 @@ const Post = ({ foodArticle }) => {
     return (
         <div className='post'>
             <h1 className='title'>{name}</h1>
-            <h3>New Changes</h3>
             {featuredImage && <img className='featuredImage' src={featuredImage.fields.file.url} alt={name} title={name} />}
             <section dangerouslySetInnerHTML={{ __html: postDescription }} />
         </div>
