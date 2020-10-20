01b6a41f2ccf0399014acdd28bc19766a62fbe34
Anvay Mandar Joshi <anvayj@cybage.com>
2020-10-20 10:52:30 +0530

    Modified Title of Recipe for Testing

--- src/components/Post.js
+++ src/components/Post.js
@@ -6,7 +6,7 @@ const Post = ({ foodArticle }) => {
     const postDescription = marked(description)
     return (
         <div className='post'>
-            <h1 className='title'>{name}</h1>
+            <h3 className='title'>{name}</h3>
             {featuredImage && <img className='featuredImage' src={featuredImage.fields.file.url} alt={name} title={name} />}
             <section dangerouslySetInnerHTML={{ __html: postDescription }} />
         </div>
