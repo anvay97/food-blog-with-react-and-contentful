bb3ee4a5a721098f38c62bc5a9db8effa8287bfe
Anvay Mandar Joshi <anvayj@cybage.com>
2020-10-27 12:51:23 +0530

    Modified UI Rendering

--- src/components/Books.js
+++ src/components/Books.js
@@ -5,7 +5,8 @@ import Book from './Book'
 const Books = ({ books, bookCount }) => {
     return (
         <div className="container" style={{marginTop: '5%'}}>
-            <h1 className="center">We have {bookCount} Books In Our Inventory</h1>
+             {bookCount > 1 ? <h1 className="center">We have {bookCount} Books </h1>
+                    : <h1 className="center">We have {bookCount} Book </h1> }
             {books.map((book, index) => <Book singleBook={book} key={index} />)}
         </div>
     )
