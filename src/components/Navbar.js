b298c4f15d39a34894587d1edb850dcea03141b6
Anvay Mandar Joshi <anvayj@cybage.com>
2020-11-08 09:41:07 +0530

    Worked on Dynamic Navigation and Detail Food page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,68 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
-import  './Navbar.css';
+import './Navbar.css';
 
-const Navbar = () => {
-  return (
-    <nav className="nav-wrapper red darken-3">
-      <div className="container">
-        <p className="brand-logo">React and Contentful</p>
-        <ul className="right" style={{marginLeft: '60%'}}>
-          <li><NavLink to="/home">Home</NavLink></li>
-          <li><NavLink to='/recipes'>Recipes</NavLink></li>
-          <li><NavLink to='/books'>Books</NavLink></li>
-          {/* <li><NavLink to='/persons'>Persons</NavLink></li> */}
+const {REACT_APP_SPACE_ID,REACT_APP_ACCESS_TOKEN} = process.env;
 
-        </ul>
-      </div>
-    </nav> 
-  )
+const query=`
+
+{
+    navigationCollection(order:[
+      sys_publishedAt_ASC
+    ]){
+      items{
+        navlink,
+        navtitle
+      }
+    }
+  }
+`;
+
+class Navbar extends Component {
+
+    state={
+        Navlinks: []
+    }
+
+    componentDidMount(){
+        window.fetch(
+            `https://graphql.contentful.com/content/v1/spaces/${REACT_APP_SPACE_ID}`,
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${REACT_APP_ACCESS_TOKEN}`
+                },
+                body: JSON.stringify({ query }),
+            }
+        ).then(res => res.json())
+            .then(({data}) =>{ 
+                console.log(data);
+                this.setState({
+                    Navlinks: data.navigationCollection.items,
+                });    
+            })
+            .catch(error=> console.log(error));
+    }
+
+    render() {
+        return (
+            <nav className="nav-wrapper red darken-3">
+                <div className="container">
+                    <p className="brand-logo">React and Contentful</p>
+                    <ul className="right" style={{ marginLeft: '60%' }}>
+                        {
+                            this.state.Navlinks.map((nav, i) => {
+                                return (
+                                    <li key={i}><NavLink to={`/${nav.navlink}`}>{nav.navtitle}</NavLink></li>
+                                )
+                            })
+                        }
+                    </ul>
+                </div>
+            </nav>
+        )
+    }
 }
 
 export default Navbar
--- src/components/Single.js
+++ src/components/Single.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { client } from '../client';
-// import marked from 'marked';
+import marked from 'marked';
 import { documentToHtmlString } from '@contentful/rich-text-html-renderer';
 import parse from 'html-react-parser';
 
@@ -10,38 +10,48 @@ class Single extends Component {
         food: null,
         des: null,
         sum: null
-    } 
+    }
 
     componentDidMount() {
         let id = this.props.match.params.food_id;
 
+
+
         client.getEntry(id).then(res => {
             localStorage.setItem('summary', JSON.stringify(res.fields.summary));
-            // console.log(res);
-           
+            localStorage.setItem('description', JSON.stringify(res.fields.description));
+
+
             this.setState({
                 food: res,
-                des: localStorage.getItem('description'),
+                des: JSON.parse(localStorage.getItem('description')),
                 sum: JSON.parse(localStorage.getItem('summary'))
             })
-            localStorage.setItem('description', this.state.food.fields.description)
         })
             .catch(err => console.log(err))
     }
-    
+
 
     render() {
 
         let summary;
-        // let description = localStorage.getItem('description');
-        // const postDescription = marked(description);
-            
-            if(this.state.sum)
-                summary = this.state.sum;
-            else
-                return(
-                    <p>No Summary</p>
-                )
+        let description;
+
+        if (this.state.sum)
+            summary = this.state.sum;
+        else
+            return (
+                <p>No Summary</p>
+            )
+
+        if (this.state.des) {
+            description = this.state.des;
+            description = marked(description);
+        }
+        else
+            return (
+                <p>No Description</p>
+            )
 
 
         const rawRichTextField = summary;
@@ -55,8 +65,8 @@ class Single extends Component {
 
                     <h1 className='title'>{this.state.food.fields.name}</h1>
                     {this.state.food.fields.featuredImage && <img className='featuredImage' src={this.state.food.fields.featuredImage.fields.file.url} alt={this.state.food.fields.name} />}
-                     
-                     {/* { <section dangerouslySetInnerHTML={{ __html: postDescription }} /> } */}
+
+                    { <section dangerouslySetInnerHTML={{ __html: description }} />}
                     <section id="rich-text-body">
                         <h1>Summary : </h1>
                         {foodSummary}
